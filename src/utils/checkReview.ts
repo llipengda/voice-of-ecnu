17bc77c3971b9b4f068a29e697ac2bd5b2aeed5c
llipengda <lipengda2@outlook.com>
2023-11-05 04:07:30 +0800

    Remove console error in checkReview function

--- src/utils/checkReview.ts
+++ src/utils/checkReview.ts
@@ -2,7 +2,6 @@ import { expectedReviewDate } from '@/common/constants'
 import Taro from '@tarojs/taro'
 
 export const checkReview = () => {
-  console.error('check review')
   const environment = Taro.getAccountInfoSync().miniProgram.envVersion
   if (environment !== 'release' && new Date() < expectedReviewDate) {
     return false
