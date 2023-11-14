4035476863447f98b8d27657face2e437021398e
llipengda <lipengda2@outlook.com>
2023-11-14 20:27:21 +0800

    Add conditional rendering for Board component based on showComponent state

--- src/pages/message/message.tsx
+++ src/pages/message/message.tsx
@@ -1,15 +1,18 @@
 import Board from '@/packages/message/components/Board/Board'
 import MessageList from '@/packages/message/components/MessageList/MessageList'
+import { useAppSelector } from '@/redux/hooks'
 import { useCheckMessage } from '@/utils/hooks/useCheckMessage'
 import { View } from '@tarojs/components'
 
 export default function Message() {
   useCheckMessage()
 
+  const showComponent = useAppSelector(state => state.review.showComponent)
+
   return (
     <View>
       <MessageList />
-      <Board />
+      {showComponent && <Board />}
     </View>
   )
 }
